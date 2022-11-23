3bdb222aa2664503352015182fcfc8d8d0c4903a
Jack <jack@realtimeradio.co.uk>
2022-11-23 17:15:47 +0000

    First commit

---
+++ slblocks.m
@@ -0,0 +1,9 @@
+function blkStruct = slblocks
+    % The name of the library
+    Browser.Library = 'rtr_casper_dsp_lib';
+
+    % The library name that appears
+    % in the Library Browser
+    Browser.Name = 'Real-Time Radio DSP Library';
+
+    blkStruct.Browser = Browser;
